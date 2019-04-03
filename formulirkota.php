e0437055874a5dcf858060ec23d66951d53e9f05
Istia Budi <istiabudi@gmail.com>
2019-04-03 21:50:29 +0800

    Update formulirkota.php

--- formulirkota.php
+++ formulirkota.php
@@ -1,5 +1,21 @@
 <?php
-    require 'function.php';
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dicodingkotabpn.database.windows.net,1433; Database = dicodingbpn", "dicodingbpn", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "dicodingbpn@dicodingkotabpn", "pwd" => "{your_password_here}", "Database" => "dicodingbpn", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dicodingkotabpn.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+//    require 'function.php';
     if (isset ($_POST['submit'])){   
         if (tambah ($_POST)> 0){
             echo "<script> 
